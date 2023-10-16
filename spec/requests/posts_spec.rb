3ee3d09dbc982a049978a015268cc5df4e90cb99
Franco Pignatelli <francopignatelli15@gmail.com>
2023-10-16 19:39:09 -0300

    Creacion de validaciones para creacion (POST) y modificacion (PUT) de un post, aun persiste el problema de 'Wrong number of arguments' al querer crear un post o una lista de posts.

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -33,8 +33,8 @@ RSpec.describe "Posts", type: :request do
     end
 
     describe "GET /posts/{id}" do
-     
-    let!(:post) { FactoryBot.create(:post) }
+
+        let!(:post) { FactoryBot.create(:post) }
 
         it "should return a post"do
             get "/posts/#{post.id}"
@@ -42,6 +42,68 @@ RSpec.describe "Posts", type: :request do
             expect(payload).to be_empty
             expect(payload["id"]).to eq(post.id)
             expect(response).to have_http_status(200)
+        end
+    end
+
+    describe "POST /posts" do
+
+        let!(:user) { create(:user) }
+
+        it "should create a post" do
+            req_payload = {
+                post: {
+                    title: "Titulo",
+                    content: 'content',
+                    published: false,
+                    user_id: user.id
+                }
+            }
+
+            #Ref al metodo post de HTTP
+            post "/posts", params: req_payload
+            payload = JSON.parse(response.body)
+            expect(payload).to_not be_empty
+            expect(payload["id"]).to_not be_empty
+            expect(response).to have_http_status(:created)
+        end
+
+        it "should return error message on invalid post" do
+            req_payload = {
+                post: {
+                    content: 'content',
+                    published: false,
+                    user_id: user.id
+                }
+            }
+
+            #Ref al metodo post de HTTP
+            post "/posts", params: req_payload
+            payload = JSON.parse(response.body)
+            expect(payload).to_not be_empty
+            expect(payload["error"]).to_not be_empty
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+    end
+
+    describe "PUT /posts/{id}" do
+        let!(:article) { create(:post) }
+
+        it "should create a post" do
+            req_payload = {
+                post: {
+                    title: "Titulo",
+                    content: 'content',
+                    published: true,
+                    user_id: user.id
+                }
+            }
+
+            #Ref al metodo post de HTTP
+            put "/posts/#{article.id}", params: req_payload
+            payload = JSON.parse(response.body)
+            expect(payload).to_not be_empty
+            expect(payload["id"]).to eq(article.id)
+            expect(response).to have_http_status(:ok)
         end
     end
 end
